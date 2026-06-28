313883877e867162e28d8ccf3da1254ccbfa69aa
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-06-28 19:58:46 -0400

    chore: reconfig neovide for new update

--- nvim/lua/neovide.lua
+++ nvim/lua/neovide.lua
@@ -1,17 +1,14 @@
 if vim.g.neovide then
   local display_mode = false
   local transparent = true
-  -- local default_font = 'JetBrainsMono Nerd Font:h11'
-  -- local display_font = 'JetBrainsMono Nerd Font:h15.5'
-  local default_font = 'FiraCode Nerd Font Mono:h11'
-  -- local default_font = 'FiraCode Nerd Font Mono:h5'
+  local default_font = 'FiraCode Nerd Font Mono:h10.5'
   local display_font = 'FiraCode Nerd Font Mono:h15.5'
 
-  vim.g.neovide_opacity = 0
-  vim.g.neovide_transparency = 0.80
-  vim.g.neovide_cursor_animation_length = 0.015
+  vim.g.neovide_opacity = 0.90
+  vim.g.neovide_cursor_animation_length = 0.045
   vim.g.neovide_refresh_rate = 144
-  vim.g.neovide_scale_factor = 1
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_scroll_animation_length = 0.35
   vim.o.guifont = default_font
 
   vim.api.nvim_create_user_command("Restart", function()
@@ -32,10 +29,10 @@ if vim.g.neovide then
   vim.keymap.set('n', '<leader>tt', function()
     if transparent then
       transparent = false
-      vim.g.neovide_transparency = 1.00
+      vim.g.neovide_opacity = 1.00
     else
       transparent = true
-      vim.g.neovide_transparency = 0.80
+      vim.g.neovide_opacity = 0.80
     end
   end, { desc = '[T]oggle [T]ransparency mode' })
 end
