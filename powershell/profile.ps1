af08ebc593ceb5fba9ce287343c786d6bf70476e
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-07-11 00:54:38 -0400

    fix: remove commented out env variables

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,30 +1,3 @@
-## path variables
-# $mojang    = Join-Path $HOME "AppData\Roaming\Minecraft Bedrock\Users\Shared\games\com.mojang"
-# $mojang2    = Join-Path $HOME "AppData\Roaming\Minecraft Bedrock\Users\15758859223355425889\games\com.mojang"
-# $samples = "C:/XboxGames/Minecraft for Windows/Content/data/"
-# $packs     = Join-Path $HOME "OneDrive\Desktop\packs"
-# $dotf      = Join-Path $HOME ".dotfiles"
-# $startup   = [Environment]::GetFolderPath("Startup")
-# $projects  = Join-Path $HOME "Projects"
-# $downloads = Join-Path $HOME "Downloads"
-# $notes     = Join-Path $HOME "Notes"
-# $pictures  = Join-Path $HOME "OneDrive\Pictures"
-# $desktop   = Join-Path $HOME "OneDrive\Desktop"
-#
-# ## for other tools
-# $env:MOJANG    = $mojang
-# $env:MOJANG2   = $mojang2
-# $env:PACKS     = $packs
-# $env:DOTF      = $dotf
-# $env:STARTUP   = $startup
-# $env:PROJECTS  = $projects
-# $env:DOWNLOADS = $downloads
-# $env:NOTES     = $notes
-# $env:PICTURES  = $pictures
-# $env:DESKTOP   = $desktop
-# $env:HOME      = $HOME
-# $env:SAMPLES   = $SAMPLES
-
 function neo
 {
     & "$HOME\.dotfiles\neo\neo.ps1"
