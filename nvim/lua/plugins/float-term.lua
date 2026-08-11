d2d8463b79db1e645bdba2310be231a7c6eedcde
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-08-11 19:04:19 -0400

    feat: float term config

--- nvim/lua/plugins/float-term.lua
+++ nvim/lua/plugins/float-term.lua
@@ -12,130 +12,67 @@ else
 end
 
 local state = {
-  floating = {
-    buf = -1,
-    win = -1,
-  },
-  prev_buf = {
-    path = nil,
-  },
+  path = nil
 }
 
-vim.api.nvim_create_autocmd('VimResized', {
-  desc = 'resize float terminal',
-  callback = function()
-    if state.floating.buf == vim.api.nvim_get_current_buf() then
-      local width = math.floor(vim.o.columns * 0.8)
-      local height = math.floor(vim.o.lines * 0.8)
-      local col = math.floor((vim.o.columns - width) / 2)
-      local row = math.floor((vim.o.lines - height) / 2)
+return {
+  "nvzone/floaterm",
+  dependencies = "nvzone/volt",
+  cmd = "FloatermToggle",
+  config = function()
+    local floaterm = require("floaterm")
+    local floaterm_state = require("floaterm.state")
+    local utils = require("floaterm.utils")
 
-      vim.api.nvim_win_set_config(state.floating.win, {
-        relative = 'editor', -- or "win", "cursor" etc, depending what you want
-        row = row,           -- new vertical position
-        col = col,           -- new horizontal position
-        width = width,
-        height = height,
-        style = 'minimal', -- No borders or extra UI elements
-        border = 'rounded',
-      })
+    local function send_cmd(opts)
+      if not floaterm_state.terminals then
+        require("floaterm").open()
+        require("floaterm.api").new_term(opts)
+      else
+        opts.cmd = type(opts.cmd) == "string" and opts.cmd or opts.cmd()
+        opts.buf = opts.buf or floaterm_state.buf
+        local bufdetails = utils.get_term_by_key(opts.buf)[2]
+
+        if opts.name then
+          bufdetails = utils.get_term_by_key(opts.name, "name")[2]
+        end
+
+        local job_id = vim.b[bufdetails.buf].terminal_job_id
+        vim.api.nvim_chan_send(job_id, opts.cmd)
+      end
     end
-  end,
-})
 
+    floaterm.setup({
+      size = { h = 80, w = 90 },
+      mappings = { sidebar = nil, term = nil },
+      terminals = {
+        { name = "Main" },
+        { name = "Build" },
+        { name = "Opencode" }
+      },
+    })
 
-vim.keymap.set('t', '<C-h>', function()
-  local term_buf = state.floating.buf
-  local chan_id = vim.b[term_buf].terminal_job_id
-  local path = state.prev_buf.path
-  local prefix = 'oil'
-
-  if not path then
-    return
-  end
-
-  if string.sub(path, 1, #prefix) == prefix then
-    path = 'C:' .. string.sub(path, 9)
-  end
-
-  vim.fn.chansend(chan_id, { 'cd "' .. path .. '"\r\n' })
-end, { noremap = true, silent = true })
-
-vim.keymap.set('t', '<C-n>', function()
-  local buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
-
-  vim.api.nvim_win_set_buf(state.floating.win, buf)
-  state.floating.buf = buf
-
-  vim.cmd.terminal()
-end, { noremap = true, silent = true })
-
-local function create_floating_window(opts)
-  opts = opts or {}
-  local width = opts.width or math.floor(vim.o.columns * 0.8)
-  local height = opts.height or math.floor(vim.o.lines * 0.8)
-  local col = math.floor((vim.o.columns - width) / 2)
-  local row = math.floor((vim.o.lines - height) / 2)
-
-  -- Create a buffer
-  local buf = nil
-  if vim.api.nvim_buf_is_valid(opts.buf) then
-    buf = opts.buf
-  else
-    buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
-  end
-
-  -- Define window configuration
-  local win_config = {
-    relative = 'editor',
-    width = width,
-    height = height,
-    col = col,
-    row = row,
-    style = 'minimal', -- No borders or extra UI elements
-    border = 'rounded',
-  }
-
-  -- Create the floating window
-  local win = vim.api.nvim_open_win(buf, true, win_config)
-
-  return { buf = buf, win = win }
-end
-
-local function toggle_terminal()
-  state.prev_buf.path = vim.fn.expand '%:p:h'
-  if not vim.api.nvim_win_is_valid(state.floating.win) then
-    state.floating = create_floating_window { buf = state.floating.buf }
-    if vim.bo[state.floating.buf].buftype ~= 'terminal' then
-      vim.cmd.terminal()
+    vim.keymap.set('n', '<C-t>', function()
+      state.path = vim.fn.expand '%:p:h'
+      floaterm.toggle()
     end
-  else
-    vim.api.nvim_win_hide(state.floating.win)
+    , { desc = '[T]oggle Terminal' })
+
+    vim.keymap.set('t', '<C-t>', floaterm.toggle, { desc = '[T]oggle Terminal' })
+
+    vim.keymap.set('t', '<C-p>', function()
+      local path = state.path
+      local prefix = 'oil'
+
+      vim.print(path)
+
+      if not path then return end
+
+      if string.sub(path, 1, #prefix) == prefix then
+        path = 'C:' .. string.sub(path, 9)
+      end
+
+      send_cmd({ cmd = 'cd "' .. path .. '"\r' })
+    end, { noremap = true, silent = true })
   end
-end
-
-vim.keymap.set('n', '<C-p>', function()
-  vim.cmd 'startinsert'
-  toggle_terminal()
-
-  local term_buf = state.floating.buf
-  local chan_id = vim.b[term_buf].terminal_job_id
-
-  if vim.o.shell == 'pwsh' or vim.o.shell == 'powershell' then
-    vim.fn.chansend(chan_id, { 'Invoke-Expression (Get-History)[-1].CommandLine\r\n' })
-  else
-    vim.fn.chansend(chan_id, { '\x1b[A' })
-  end
-end, { noremap = true, silent = true, desc = 'run most recent shell command' })
-
--- Map Ctrl-t in normal and terminal modes
-vim.keymap.set('n', '<C-t>', function()
-  vim.cmd 'startinsert'
-  toggle_terminal()
-end, { desc = '[T]oggle Terminal' })
-vim.keymap.set('t', '<C-t>', function()
-  vim.cmd 'stopinsert'
-  toggle_terminal()
-end, { desc = '[T]oggle Terminal' })
-
-return {}
+}
